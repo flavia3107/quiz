dc31c2901a1a6488a6ff407d64e9ca8395ee4067
Flavia Kosti <flaviakosti@gmail.com>
2023-08-11 21:46:36 +0200

    Quiz: Generate dynamic content

---
+++ configuration/configuration.js
@@ -0,0 +1,22 @@
+console.log('config', categories);
+
+const categoryList = document.getElementById('category');
+
+
+function appendCategories() {
+    categories.forEach(category => {
+        const optionElement = document.createElement("li");
+        // optionElement.text = `${category.name}`;
+        optionElement.value = `${category.id}`;
+        optionElement.classList = 'category-item';
+        optionElement.innerHTML = `
+        <i class="fa-solid fa-heart"></i>
+            <span class="category-name">${category.name}</span>
+        `
+        categoryList.appendChild(optionElement);
+    });
+}
+
+
+
+appendCategories();
